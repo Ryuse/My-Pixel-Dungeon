28d10975217eadba055434c0d7ef1abf63ec3fb1
Ryuse <leemuel01234@gmail.com>
2020-11-30 01:12:19 +0800

    Setup

---
+++ android/build/generated/source/buildConfig/debug/com/shatteredpixel/shatteredpixeldungeon/android/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.shatteredpixel.shatteredpixeldungeon.android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.shatteredpixel.shatteredpixeldungeon.indev";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 490;
+  public static final String VERSION_NAME = "0.9.0b-INDEV";
+}
